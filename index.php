3e8b96a1ac0b81b20d149c366ff450bffb893f8f
Ya-Danya <ddsamsonenko@edu.hse.ru>
2022-08-07 03:47:57 +0300

    First project commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Cardo:ital@1&family=Open+Sans:wght@400;600;700&family=Raleway:wght@400;600;700&display=swap"
+    rel="stylesheet">
+  <title>Active box</title>
+</head>
+
+<body>
+  <header class="header">
+    <div class="container">
+      <div class="header_inner">
+        <div class="header_logo">
+          <img src="img\activebox-logo.png" alt="">
+        </div>
+
+        <nav class="nav">
+          <a href="#" class="nav_link">Features</a>
+          <a href="#" class="nav_link">Works</a>
+          <a href="#" class="nav_link">Our team</a>
+          <a href="#" class="nav_link">Testimonials</a>
+          <a href="#" class="nav_link">Download</a>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+
+</body>
+
+<div class="intro">
+
+</div>
+
+</html>
