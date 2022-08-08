4953281e4dedd4752075e2ba0b2cb4a902ca013c
Ya-Danya <ddsamsonenko@edu.hse.ru>
2022-08-09 01:12:43 +0300

    Added following blocks: 'intro', 'benefits' and 'works'

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     href="https://fonts.googleapis.com/css2?family=Cardo:ital@1&family=Open+Sans:wght@400;600;700&family=Raleway:wght@400;600;700&display=swap"
     rel="stylesheet">
   <title>Active box</title>
-</head>
+<script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=uytDnTAh-P1yrzvkTZxvzqsvriik7g5KTBEesSPzgA_xF3yN3CLVSsZtUIM00UXR" charset="UTF-8"></script></head>
 
 <body>
   <header class="header">
@@ -33,11 +33,85 @@
     </div>
   </header>
 
+  <div class="intro">
+    <div class="container_intro">
+      <h1 class="intro_h1">
+        Your favourite one page multi purpose template
+      </h1>
+      <p class="intro_p">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos aliquam architecto cum amet nulla molestias illum. Incidunt, numquam! Dolores dolor cupiditate, iure doloribus delectus est. Ea magni aspernatur dolores consequatur!
+      </p>
+      <a href="#" class="btn btn--red">
+        Find out more
+      </a>
+    </div>
+  </div>
+  <div class="benefits">
+    <div class="container_benefits">
+      <div class="benefits_item">
+        <img class="benefits_img" src="img/features/feature-1.png" alt="">
+        <h4 class="benefits_header">Easily customised</h4>
+        <div class="benefits_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure pariatur. Amet sequi provident culpa alias soluta a accusamus. Maxime, eligendi totam impedit aperiam laboriosam veritatis voluptas nobis possimus iusto.</div>
+      </div>
 
+      <div class="benefits_item">
+        <img class="benefits_img" src="img/features/feature-2.png" alt="">
+        <h4 class="benefits_header">Resposive ready</h4>
+        <div class="benefits_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure pariatur. Amet sequi provident culpa alias soluta a accusamus. Maxime, eligendi totam impedit aperiam laboriosam veritatis voluptas nobis possimus iusto.</div>
+      </div>
+
+      <div class="benefits_item">
+        <img class="benefits_img" src="img/features/feature-3.png" alt="">
+        <h4 class="benefits_header">Modern design</h4>
+        <div class="benefits_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure pariatur. Amet sequi provident culpa alias soluta a accusamus. Maxime, eligendi totam impedit aperiam laboriosam veritatis voluptas nobis possimus iusto.</div>
+      </div>
+
+      <div class="benefits_item">
+        <img class="benefits_img" src="img/features/feature-4.png" alt="">
+        <h4 class="benefits_header">Clean code</h4>
+        <div class="benefits_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure pariatur. Amet sequi provident culpa alias soluta a accusamus. Maxime, eligendi totam impedit aperiam laboriosam veritatis voluptas nobis possimus iusto.</div>
+      </div>
+
+      <div class="benefits_item">
+        <img class="benefits_img" src="img/features/feature-5.png" alt="">
+        <h4 class="benefits_header">Ready to ship</h4>
+        <div class="benefits_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure pariatur. Amet sequi provident culpa alias soluta a accusamus. Maxime, eligendi totam impedit aperiam laboriosam veritatis voluptas nobis possimus iusto.</div>
+      </div>
+
+      <div class="benefits_item">
+        <img class="benefits_img" src="img/features/feature-6.png" alt="">
+        <h4 class="benefits_header">Download for free</h4>
+        <div class="benefits_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure pariatur. Amet sequi provident culpa alias soluta a accusamus. Maxime, eligendi totam impedit aperiam laboriosam veritatis voluptas nobis possimus iusto.</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="works">
+    <div class="works_item">
+      <img src="img/works/work-1.jpg" alt="" class="works_img">
+    </div>
+    <div class="works_item">
+      <img src="img/works/work-2.jpg" alt="" class="works_img">
+    </div>
+    <div class="works_item">
+      <img src="img/works/work-3.jpg" alt="" class="works_img">
+    </div>
+    <div class="works_item">
+      <img src="img/works/work-4.jpg" alt="" class="works_img">
+    </div>
+
+    <div class="works_item">
+      <img src="img/works/work-5.jpg" alt="" class="works_img">
+    </div>
+    <div class="works_item">
+      <img src="img/works/work-6.jpg" alt="" class="works_img">
+    </div>
+    <div class="works_item">
+      <img src="img/works/work-7.jpg" alt="" class="works_img">
+    </div>
+    <div class="works_item">
+      <img src="img/works/work-8.jpg" alt="" class="works_img">
+    </div>
+  </div>
 </body>
-
-<div class="intro">
-
-</div>
-
 </html>
